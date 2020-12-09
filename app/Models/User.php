a93f011d759a03d9b3bca323cbf70846cebd34b7
HijenHek <k.hijen@gmail.com>
2020-12-09 20:42:19 +0100

    friends query ...

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,8 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable
 {
@@ -56,5 +58,25 @@ class User extends Authenticatable
     
     public function discussions(){
         return $this->belongsToMany(Discussion::class);
+    }
+
+
+    public function friends() {
+        $one = DB::table('friends')->join('users',  'friends.user_id', '=', 'users.id')
+                            ->select('users.*')
+                            ->where('friends.user_id', Auth::id())
+                            ->where('users.id' ,'<>' , Auth::id())
+                            ->get();
+
+        return DB::table('friends')->join('users',  'friends.user_id', '=', 'users.id')
+                        ->select('users.*')
+                        ->where('friends.friend_id', Auth::id())
+                        ->where('users.id' ,'<>' , Auth::id())
+                        ->union($one)
+                        ->get();
+        // select * from friends f , users u where f.user_id = 11  
+        // and f.user_id = u.id
+        // and u.id <> 11 
+        // UNION select * from friends f , users u where f.friend_id = 11 and f.user_id = u.id and u.id <> 11
     }
 }
---
+++ database/migrations/2020_12_09_183141_create_friends_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->unsignedBigInteger('friend_id');
+            $table->timestamps();
+            $table->boolean('friends')->default(false);
+            $table->foreign('friend_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
